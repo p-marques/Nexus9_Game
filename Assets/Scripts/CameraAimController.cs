2598455192af785a832795bb988e4bdbdd3f7e13
Mikapuccino <nelsonfcmilheiro@gmail.com>
2020-11-26 19:41:02 +0000

    Add Player Movement
    Add Camera Control

---
+++ Assets/Scripts/CameraAimController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAimController : MonoBehaviour
+{
+    [SerializeField]
+    [Range(10, 300)]
+    private float mouseSensitivity = 230f;
+    [SerializeField]
+    private float maxLookUpRotation = 80f;
+    [SerializeField]
+    private float maxLookDownRotation = 60f;
+    
+    private Transform parentTransform;
+
+    private float inputMouseX;
+    private float inputMouseY;
+
+    private void Awake()
+    {
+        parentTransform = gameObject.transform.parent;
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void FixedUpdate()
+    {
+        HandleCameraPitch();
+
+        HandleParentRotation();
+    }
+
+    private void Update()
+    {
+        inputMouseX = Input.GetAxis("Mouse X");
+        inputMouseY = Input.GetAxis("Mouse Y");
+    }
+
+    private void HandleCameraPitch()
+    {
+        Vector3 cameraRotation = transform.localRotation.eulerAngles;
+        
+        cameraRotation.x -= inputMouseY * mouseSensitivity * Time.fixedDeltaTime;
+
+        if (cameraRotation.x > 180f)
+        {
+            cameraRotation.x = Mathf.Max(cameraRotation.x, 360f - maxLookUpRotation);
+        }
+        else
+        {
+            cameraRotation.x = Mathf.Min(cameraRotation.x, maxLookDownRotation);
+        }
+
+        transform.localRotation = Quaternion.Euler(cameraRotation.x, 0f, 0f);
+    }
+
+    private void HandleParentRotation()
+    {
+        parentTransform.Rotate(Vector3.up * inputMouseX * Time.fixedDeltaTime * mouseSensitivity);
+    }
+}
