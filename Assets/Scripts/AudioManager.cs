14742ca5632199d1f5409677b786fc4bc2bc465c
Pedro Dias Marques <pmark.pt@gmail.com>
2020-12-16 15:47:56 +0000

    Add audio feedback to blocked action
    Add prefabs for repeatable elements

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+}
